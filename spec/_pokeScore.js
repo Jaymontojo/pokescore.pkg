8d21600a502961057b15ddd21e7dbb7f49b0215e
Jay <julesmontojo1@gmail.com>
2022-04-03 05:14:17 +0900

    refactor: update PokeScore to be a class

--- spec/_pokeScore.js
+++ spec/_pokeScore.js
@@ -1,30 +1,30 @@
 const PokeScore = require("../src/pokeScore");
 const {pokemon, attacks, types} = require("../data/index");
 const { expect } = require("chai");
-const { identity, sanitize, isString, isNumber, findPokemon } = PokeScore;
+//const { identity, sanitize, isString, isNumber, findPokemon } = new PokeScore;
 
 describe("PokeScore", () => {
   describe("identity", () => {
     it('should exist as a method on the PokeScore object', () => {
-      expect(identity).to.exist;
-      expect(typeof identity).to.equal("function")
+      expect(PokeScore.identity).to.exist;
+      expect(typeof PokeScore.identity).to.equal("function")
     });
   });
 
   describe("sanitize", () => {
     it('should exist as a method on the PokeScore object', () => {
-      expect(sanitize).to.exist;
-      expect(typeof sanitize).to.equal("function");
+      expect(PokeScore.sanitize).to.exist;
+      expect(typeof PokeScore.sanitize).to.equal("function");
     });
 
     it('should take an input and return as string in lowercase if input is not a stringified number', () => {
-      const result = sanitize("Philip J. Fry");
+      const result = PokeScore.sanitize("Philip J. Fry");
       expect(typeof result).to.equal("string");
       expect(result).to.equal("philip j. fry");
     });
 
     it('should take an input and return as parsed number if the input is a stringified number', () => {
-      const result = sanitize("001");
+      const result = PokeScore.sanitize("001");
       expect(typeof result).to.equal("number");
       expect(result).to.equal(1);
     });
@@ -32,18 +32,18 @@ describe("PokeScore", () => {
 
   describe("isString", () => {
     it('should exist as a method on the PokeScore object', () => {
-      expect(isString).to.exist;
-      expect(typeof isString).to.equal("function");
+      expect(PokeScore.isString).to.exist;
+      expect(typeof PokeScore.isString).to.equal("function");
     });
 
     it('should take an input and return true if it is a string', () => {
-      const result = isString("bulbasaur");
+      const result = PokeScore.isString("bulbasaur");
       expect(typeof result).to.equal("boolean");
       expect(result).to.equal(true);
     });
 
     it('should take an input and return false if it is not', () => {
-      const result = isString(1456);
+      const result = PokeScore.isString(1456);
       expect(typeof result).to.equal("boolean");
       expect(result).to.equal(false);
     });
@@ -51,41 +51,41 @@ describe("PokeScore", () => {
 
   describe("isNumber", () => {
     it('should exist as a method on the PokeScore object', () => {
-      expect(isString).to.exist;
-      expect(typeof isString).to.equal("function");
+      expect(PokeScore.isString).to.exist;
+      expect(typeof PokeScore.isString).to.equal("function");
     });
 
     it('should take an input and return true if it is a number', () => {
-      const result = isNumber(11223);
+      const result = PokeScore.isNumber(11223);
       expect(typeof result).to.equal("boolean");
       expect(result).to.equal(true);
     });
 
     it('should take an input and return false if it is not', () => {
-      const result = isNumber("Tauranga Leela");
+      const result = PokeScore.isNumber("Tauranga Leela");
       expect(typeof result).to.equal("boolean");
       expect(result).to.equal(false);
     });
   });
 
-  describe.only("find", ()=>{
+  describe("find", ()=>{
     it('should exist as a method on the PokeScore object', () => {
-      expect(findPokemon).to.exist;
-      expect(typeof findPokemon).to.equal("function");
+      expect(PokeScore.findPokemon).to.exist;
+      expect(typeof PokeScore.findPokemon).to.equal("function");
     });
 
     it('should return an array of pokemon objects when given a params object with id (integer)', () => {
-      const result = findPokemon(pokemon, {id: 1});
+      const result = PokeScore.findPokemon(pokemon, {id: 1});
       expect(result[0]).to.deep.equal(pokemon[0]);
     });
 
     it('should return an array of pokemon objects when given a params object with id (string)', () => {
-      const result = findPokemon(pokemon, {id: "001"});
+      const result = PokeScore.findPokemon(pokemon, {id: "001"});
       expect(result[0]).to.deep.equal(pokemon[0]);
     });
 
     it('should return an array of pokemon objects when given a params object with name (string)', () => {
-      const result = findPokemon(pokemon, {name: "bulbasaur"});
+      const result = PokeScore.findPokemon(pokemon, {name: "bulbasaur"});
       expect(result[0]).to.deep.equal(pokemon[0]);
     });
   })
--- src/pokeScore.js
+++ src/pokeScore.js
@@ -1,41 +1,41 @@
 //refac: as a class
 //refac: convert to typescript
-const PokeScore = {
+class PokeScore {
   identity(val) {
     return val;
-  },
+  };
   //refac: private to this class so maybe extend from a utils class(?)
   sanitize(input) {
     if(isNaN(input)) return input.toLowerCase(); //"philip j. fry"
     else return parseInt(input);
-  },
+  };
 
   //refac: private to this class so maybe extend from a utils class
   isString(idOrName) {
     if(typeof idOrName === "string") return true;
     else return false
-  },
+  };
 
   //refac: private to this class so maybe extend from a utils class
   isNumber(idOrName) {
     if(typeof idOrName === "number") return true;
     else return false
-  },
+  };
   
   //takes collection * prams obj ex {name: "BuLbAsAuR"}
   //returns array of results that match.
-  findPokemon(collection, paramsObj){
+  findPokemon(collection, paramsObj) {
     const pKey = Object.keys(paramsObj)[0];
-    const pVal = PokeScore.sanitize(paramsObj[pKey])
-    if(PokeScore.isString(pVal)) {
+    const pVal = this.sanitize(paramsObj[pKey])
+    if(this.isString(pVal)) {
       return collection.filter(el => el[pKey].toLowerCase() === pVal);
     }; 
-    if(PokeScore.isNumber(pVal)) {
+    if(this.isNumber(pVal)) {
       return collection.filter(el => parseInt(el[pKey]) === pVal);
     };
     return undefined;
-  }
+  };
 };
 
-module.exports = PokeScore;
+module.exports = new PokeScore();
 
